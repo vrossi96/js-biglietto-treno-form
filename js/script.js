59f79b6496f3cbfcda07bd5a96fd7ada39677631
vrossi96 <vrossi396@yahoo.it>
2022-01-12 14:08:13 +0100

    added inputs for name and distance

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log('JS ok');
+
+/* 
+Il programma dovrà mostrare una form da compilare con cui chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo).
+
+1-Chiedo nome e cognome
+2-Chiedo i km da percorrere
+*/
