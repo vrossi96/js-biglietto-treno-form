06034a3c36bc6f0e283f32894107264b6d7efa8e
vrossi96 <vrossi396@yahoo.it>
2022-01-12 15:13:22 +0100

    added price generated by clicking

--- js/script.js
+++ js/script.js
@@ -14,21 +14,46 @@ Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato
 */
 
 //* Chiedo i dati
-const userName = document.getElementById('name');
-const userDistance = document.getElementById('distance');
-const userAge = document.getElementById('age');
+const userName = document.getElementById('name-input');
+const userDistance = document.getElementById('distance-input');
+const userAge = document.getElementById('age-input');
+const buttonGenerate = document.getElementById('generate');
+const buttonClear = document.getElementById('clear');
 
 console.log('User Name: ', userName.value);
 console.log('User KM: ', userDistance.value);
 console.log('User Age: ', userAge.value);
 
 
-//* Calcolo prezzo biglietto
-const ticketPrice = userDistance.value * 0.21;
-console.log(ticketPrice);
+//* Creo variabili per il prezzo del biglietto
+let ticketPrice;
+let finalPrice;
 
+// Creo evento al click con calcolo prezzo e stampa
+buttonGenerate.addEventListener ('click', function() {
+// Calcolo prezzo iniziale del biglietto 
+let ticketPrice = userDistance.value * 0.21;
+let finalPrice;
+// Controllo l'età del passegero
 if (userAge.value == 'no-age') {
    alert("Seleziona l'età");
 } else if (userAge.value == 'under-age') {
-
+   finalPrice = ticketPrice * 0.8;
+} else if (userAge.value == 'old-age') {
+   finalPrice = ticketPrice * 0.6;
+} else {
+   finalPrice = ticketPrice;
 }
+// Controllo che in console sia tutto giusto
+console.log('User Name: ', userName.value);
+console.log('User KM: ', userDistance.value);
+console.log('User Age: ', userAge.value);
+console.log('Ticket price: ', finalPrice);
+})
+
+// Resetto i campi di input
+buttonClear.addEventListener ('click', function() {
+userName.value = '';
+userDistance.value = '';
+userAge.value = 'no-age';
+})
